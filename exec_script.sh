7def77c1b61d6b34f4ee1671829563a4c45722ab
Rogerio Figueredo <rfigsousa@gmail.com>
2021-02-09 21:25:24 -0300

    shell

---
+++ exec_script.sh
@@ -0,0 +1,9 @@
+python script_classification.py apps naive_bayes exps_features/model/ exps_features/result/
+python script_classification.py apps svm exps_features/model/ exps_features/result/
+python script_classification.py apps tree exps_features/model/ exps_features/result/
+python script_classification.py apps nn exps_features/model/ exps_features/result/
+
+python script_classification.py movies naive_bayes exps_features/model/ exps_features/result/
+python script_classification.py movies svm exps_features/model/ exps_features/result/
+python script_classification.py movies tree exps_features/model/ exps_features/result/
+python script_classification.py movies nn exps_features/model/ exps_features/result/
