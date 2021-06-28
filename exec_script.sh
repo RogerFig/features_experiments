6e9524ecfa28209f48787a654fa0cab6d2f2935b
Rogerio Figueredo <rfigsousa@gmail.com>
2021-06-28 10:34:48 -0300

    Shell Script for batch exec

--- exec_script.sh
+++ exec_script.sh
@@ -1,8 +1,22 @@
-python script_classification.py apps oner exps_features/model/ exps_features/result/ 
-python script_classification.py apps dummy exps_features/model/ exps_features/result/ 
+python tfidf_classifier.py apps naive_bayes exps_features/result/ tf
+python tfidf_classifier.py apps tree exps_features/result/ tf
+python tfidf_classifier.py apps randfor exps_features/result/ tf
+python tfidf_classifier.py apps svm exps_features/result/ tf
+python tfidf_classifier.py apps nn exps_features/result/ tf
 
-python script_classification.py movies oner exps_features/model/ exps_features/result/ 
-python script_classification.py movies dummy exps_features/model/ exps_features/result/ 
+python tfidf_classifier.py movies naive_bayes exps_features/result/ tfidf
+python tfidf_classifier.py movies tree exps_features/result/ tfidf
+python tfidf_classifier.py movies randfor exps_features/result/ tfidf
+python tfidf_classifier.py movies svm exps_features/result/ tfidf
+python tfidf_classifier.py movies nn exps_features/result/ tfidf
+
+
+
+# python script_classification.py apps oner exps_features/model/ exps_features/result/ 
+# python script_classification.py apps dummy exps_features/model/ exps_features/result/ 
+
+# python script_classification.py movies oner exps_features/model/ exps_features/result/ 
+# python script_classification.py movies dummy exps_features/model/ exps_features/result/ 
 
 # python script_classification.py apps naive_bayes exps_features/model/ exps_features/result/ ig
 # python script_classification.py apps svm exps_features/model/ exps_features/result/ ig
